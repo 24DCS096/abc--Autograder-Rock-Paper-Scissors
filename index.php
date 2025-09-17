e92ce5abdefc54323dc2d9bd6af80c3ee7ac0138
Yash Rakeshbhai Patel <24dcs096@charusat.edu.in>
2025-09-17 18:49:48 +0530

    index.php
    abc- Autograder: Rock Paper Scissors

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Rock Paper Scissors - ba63c271</title>
+</head>
+<body>
+    <h1>Welcome to Rock Paper Scissors</h1>
+    <p><a href="game.php?name=YourName">Please Log In</a></p>
+</body>
+</html>
